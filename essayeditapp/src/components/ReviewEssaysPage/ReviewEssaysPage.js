4e08f685d3ffbe232aa3240b62ecfd6937aae687
William Wang <billwang115@gmail.com>
2021-11-06 00:06:25 -0400

    Added search bar to review essays page

--- essayeditapp/src/components/ReviewEssaysPage/ReviewEssaysPage.js
+++ essayeditapp/src/components/ReviewEssaysPage/ReviewEssaysPage.js
@@ -1,7 +1,25 @@
 import styles from "./ReviewEssaysPage.module.css";
+import creditIcon from "../../assets/credit_icon.svg";
+
+const numCredits = 15; //this value will be retrieved from a server call
 
 const ReviewEssaysPage = () => {
-  return <div className={styles.ReviewEssaysContainer}></div>;
+  return (
+    <div className={styles.ReviewEssaysContainer}>
+      <div className={styles.totalCreditsContainer}>
+        <img src={creditIcon} alt="credit-icon" className={styles.creditIcon} />
+        <span className={styles.creditText}>{numCredits} Credits</span>
+      </div>
+
+      <div className={styles.searchBarContainer}>
+        <input
+          type="text"
+          placeholder="Search Essays"
+          className={styles.searchBar}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ReviewEssaysPage;
