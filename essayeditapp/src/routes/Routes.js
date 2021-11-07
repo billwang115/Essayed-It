2bd96524fd1a5ff95f7dc92dcafd936b18e28dfa
William Wang <billwang115@gmail.com>
2021-11-07 17:28:04 -0500

    Fixed router bug

---
+++ essayeditapp/src/routes/Routes.js
@@ -0,0 +1,71 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import Header from "../components/Header/Header";
+import WelcomePage from "../components/WelcomePage/WelcomePage";
+import ProfilePage from "../components/ProfilePage/ProfilePage";
+import YourRequestsPage from "../components/YourRequests/YourRequests";
+import ReviewRequest from "../components/ReviewRequest/ReviewRequest";
+import EditPage from "../components/EditPage/EditPage";
+import ReviewEssaysPage from "../components/ReviewEssaysPage/ReviewEssaysPage";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+
+        <Route
+          path="reviewEssays"
+          element={
+            <PrivateRoute>
+              <Header />
+              <ReviewEssaysPage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="profile"
+          element={
+            <PrivateRoute>
+              <Header />
+              <ProfilePage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="yourRequests"
+          element={
+            <PrivateRoute>
+              <Header />
+              <YourRequestsPage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="Request"
+          element={
+            <PrivateRoute>
+              <Header />
+              <ReviewRequest />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="Editor"
+          element={
+            <PrivateRoute>
+              <Header />
+              <EditPage />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
