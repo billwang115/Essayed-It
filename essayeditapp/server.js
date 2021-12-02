0e375c083d7d23d72df5d99599abd80b93e6163a
moraneth <ethan.moran@mail.utoronto.ca>
2021-12-02 14:31:12 -0500

    Added express and mongoose implementations to app

---
+++ essayeditapp/server.js
@@ -0,0 +1,22 @@
+/* E4 server.js */
+'use strict';
+const log = console.log;
+
+// Express
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+
+// Mongo and Mongoose
+const { ObjectID } = require('mongodb')
+const { mongoose } = require('./db/mongoose');
+
+
+
+
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+});
