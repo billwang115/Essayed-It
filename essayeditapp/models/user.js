b4ed48177c17f59dc654464ba97979bcb11bfe9a
William Wang <lilmoseby208@gmail.com>
2021-12-03 12:23:55 -0500

    created user model

---
+++ essayeditapp/models/user.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 4,
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isAlphanumeric,
+      message: "can only contain alphanumeric characters",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 4,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  // checks to ensure we don't hash password more than once
+  if (this.isModified("password")) {
+    // generate salt and hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(this.password, salt);
+    this.password = hash;
+    next();
+  } else {
+    next();
+  }
+});
+
+UserSchema.statics.findUserByCred = async function (username, password) {
+  const foundUser = await this.findOne({ username: username });
+
+  if (!foundUser) {
+    return Promise.reject("no user found");
+  }
+
+  const isMatch = await bcrypt.compare(password, this.password);
+  return isMatch
+    ? Promise.resolve(foundUser)
+    : Promise.reject("Passwords do not match");
+};
+
+const User = mongoose.model("User", UserSchema);
+module.exports = { User };
